96711f6964c07c7984f234b550c9cc8d98552248
23046ys <23046ys@hvhs.school.nz>
2026-03-06 09:58:26 +1300

    Made enemy stages work and started trying to make door mechanic function correctly

--- gameplay.js
+++ gameplay.js
@@ -7,10 +7,13 @@
 /*******************************************************/
 // setup()
 /*******************************************************/
+
+const TIMER_SPEED= 1/50;
+
 var score= 0;
 var enemyStage= 1;
-let doorClosed= false;
 
+let doorClosed= false;
 
 
 function setup() {
@@ -18,15 +21,49 @@ function setup() {
 	cnv = new Canvas(((windowWidth/6)*4), windowHeight);
 	timer= new Sprite(0,0,10,10)
     timer.color= 'ccc';
-    timer.vel.y=1;
+    timer.vel.y= TIMER_SPEED;
     enemyMovement= new Sprite(0,0,10,10)
     enemyMovement.vel.x= 1;
 }
 
+
+
+function enemyMove(){
+
+    if(enemyMovement.position.x>=100){
+        if(enemyStage==3){
+            console.log("Enemy Attacking");
+             if(doorclosed== true){                
+                enemyMovement.position.x= 0;
+                enemyStage= 1;
+             }else{
+                endGame()
+             }
+
+       }else{
+            enemyStage= enemyStage+1;
+            enemyMovement.position.x= 0;
+       }
+
+    }
+}
+
+
+
+function doorControl(){
+    if(kb.pressing('spacebar')){
+        doorClosed= true;
+    }else{
+        doorClosed= false;
+    }
+}
+
+
+
 function endGame(){
     timer.vel.y=0;
     enemyMovement.vel.x=0;
-
+    console.log("Game Ended");
 }
 	
 /*******************************************************/
@@ -38,30 +75,14 @@ function draw() {
 
 
 
-    score= Math.floor(timer.position.y/50);
+    score= Math.round(timer.position.y);
     text(score, 50, 50)
     text(enemyStage, 100, 100)
+    text(doorClosed, 200, 200)
 
+    doorControl()
 
-
-
-
-    if(enemyMovement.position.x<=1000){
-        if(enemyStage==3){
-             if(doorclosed== true){
-                enemyStage= 1;
-         }else{
-            endGame()
-         }
-            enemyMovement.position.x= 0;
-       }else{
-            enemyStage= 2;
-            enemyMovement.position.x= 0;
-       }
-
-    }
-
-    
+    enemyMove()
 
 
 
